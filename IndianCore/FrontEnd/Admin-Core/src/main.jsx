171f82b29fede340c16b1fdeb00951d77118dabb
Vignesh K <vigneshk@ITAdmins-MacBook-Pro.local>
2025-05-25 19:41:49 +0530

    Changes

--- IndianCore/FrontEnd/Admin-Core/src/main.jsx
+++ IndianCore/FrontEnd/Admin-Core/src/main.jsx
@@ -6,7 +6,6 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  
   <StrictMode>
     <BrowserRouter>
       <Routes>
